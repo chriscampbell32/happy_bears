f1acca646d72668e0acd8de617ce08b097af778b
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-30 12:07:12 -0500

    reads the info from database

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ connection.query('SELECT name, favorite_food, personality FROM happy_bears;', fu
   if (err) throw err;
  
   for (var i = 0; i < rows.length; i++){
-          console.log(rows[i].name);
+          console.log(rows[i].name + " loves to eat " + rows[i].favorite_food + " and he is kind of " + rows[i].personality);
   }
 
 });
